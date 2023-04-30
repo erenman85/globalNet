8b8438fa4313b161a2043f4f67c3fbd36de9150d
erentsen <erecenmancaev@gmail.com>
2023-04-30 19:00:10 +0300

    add files module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img src='https://e7.pngegg.com/pngimages/184/387/png-clipart-black-earth-illustration-globe-world-map-computer-icons-global-miscellaneous-monochrome.png'/>
+    <header className={classes.header}>
+      <img className={classes.logo} src='https://e7.pngegg.com/pngimages/184/387/png-clipart-black-earth-illustration-globe-world-map-computer-icons-global-miscellaneous-monochrome.png'/>
     </header>
   )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className='main-content'>
+    <div className={classes.content}>
+       <div>
+         <img className={classes.banner} src='http://blogtravel.by/wp-content/uploads/2021/11/dostoprimechatelnosti-venezii-34.jpg'/>
+       </div>
           <div>
-            <img className='banner' src='http://blogtravel.by/wp-content/uploads/2021/11/dostoprimechatelnosti-venezii-34.jpg'/>
-          </div>
-          <div>
-          <img className='avatar' src='https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'/>
+          <img className={classes.avatar} src='https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'/>
           <div>desription</div>
           </div>
           <div>my post
-            <div>New post</div>
-            <div>post1</div>
-            <div>post2</div>
+            <div>
+              New post
+            </div>
+           <div className={classes.posts}>
+              <div className={classes.item}>post1</div>
+              <div className={classes.item}>post2</div>
+            </div>
           </div>
-          </div>
+     </div>
   )
 }
 export default Profile;
