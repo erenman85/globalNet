3239166840c5a662ab1768ca498a371ea55cbaee
erentsen <erecenmancaev@gmail.com>
2023-07-07 15:16:40 +0300

    refactoring code in files state.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from './redax/state';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './redax/state';
+import store from './redax/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
- let rerenderEntireTree = (state) => {
+ let rerenderEntireTree = (_state) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-      <App state= {state} addPost= {addPost} updateNewPostText = {updateNewPostText} 
-       updateNewMessageText = {updateNewMessageText}
-       addMessage = {addMessage}
+      <App state= {_state} addPost= {store.addPost.bind(store)} updateNewPostText = {store.updateNewPostText.bind(store)} 
+       updateNewMessageText = {store.updateNewMessageText.bind(store)}
+       addMessage = {store.addMessage.bind(store)}
       />
       </BrowserRouter>
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redax/state.js
+++ src/redax/state.js
@@ -1,79 +1,95 @@
-let rerenderEntireTree = () => {
-  console.log('state changed')
-};
-
-
-const state = {
-  profilePages: {
-    posts: [{ id: 1, message: 'Hi,How are you?', likeCount: 45 },
-    { id: 2, message: "It's my first post", likeCount: 12 } ],
-    newPostText: "new post.."
-  },
-  dialogsPages: {
-    dialogs:  [
-      {id : 1, name: 'Erentsen', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 2, name: 'Saglara', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 3, name: 'Chingis', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 4, name: 'Mama', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 5, name: 'Papa', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'}
-    ],
-      messages: [
-        {id:1, message : 'Hello'},
-        {id:2, message : 'What`s up??'},
-        {id:3, message : 'Caramba!!!'},
-        {id:4, message : 'YO!'}
+let store = {
+   _state : {
+    profilePages: {
+      posts: [{ id: 1, message: 'Hi,How are you?', likeCount: 45 },
+      { id: 2, message: "It's my first post", likeCount: 12 } ],
+      newPostText: "new post.."
+    },
+    dialogsPages: {
+      dialogs:  [
+        {id : 1, name: 'Erentsen', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 2, name: 'Saglara', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 3, name: 'Chingis', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 4, name: 'Mama', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 5, name: 'Papa', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'}
       ],
-      newMessageText: "new message.."
+        messages: [
+          {id:1, message : 'Hello'},
+          {id:2, message : 'What`s up??'},
+          {id:3, message : 'Caramba!!!'},
+          {id:4, message : 'YO!'}
+        ],
+        newMessageText: "new message.."
+    },
+  
+    sidebarPages : {
+      friends: [
+        {id : 1, name: 'Erentsen', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 2, name: 'Saglara', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 3, name: 'Chingis', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 4, name: 'Mama', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
+        {id : 5, name: 'Papa', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'}
+      ]
+    }
+  
   },
+  getState () {
+    return this._state;
+  },
+   _callSubsriber () {
+    console.log('state changed')
+  },
+  subscribe (observer) {
+    this._callSubsriber = observer;
+  
+  },
+  addPost () {
+    let newPost = {
+      id:5,
+      message:this._state.profilePages.newPostText,
+      likeCount:1
+    };
+    this._state.profilePages.posts.push(newPost);
+    this._state.profilePages.newPostText = '';
+    this._callSubsriber(this._state);
+  },
+  updateNewPostText (newText) {
+    this._state.profilePages.newPostText = newText;
+    this._callSubsriber(this._state);
+  
+  },
+  addMessage () {
+    let newMessage = {
+      id:5,
+      message:this._state.dialogsPages.newMessageText
+    };
+    this._state.dialogsPages.messages.push(newMessage);
+    this._state.dialogsPages.newMessageText = '';
+    this._callSubsriber(this._state);
+  },
+  updateNewMessageText (text) {
+    this._state.dialogsPages.newMessageText = text;
+    this._callSubsriber(this._state);
+  },
+   dispatch (action) {
 
-  sidebarPages : {
-    friends: [
-      {id : 1, name: 'Erentsen', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 2, name: 'Saglara', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 3, name: 'Chingis', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 4, name: 'Mama', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 5, name: 'Papa', avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'}
-    ]
-  }
+   }
 
-}
-window.state = state;
-
-export let addPost = () => {
-  let newPost = {
-    id:5,
-    message:state.profilePages.newPostText,
-    likeCount:1
-  };
-  state.profilePages.posts.push(newPost);
-  state.profilePages.newPostText = '';
-  rerenderEntireTree(state);
-};
-export let updateNewPostText = (newText) => {
-  state.profilePages.newPostText = newText;
-  rerenderEntireTree(state);
-
-}
-
-export let addMessage = () =>{
-  let newMessage = {
-    id:5,
-    message:state.dialogsPages.newMessageText
-  };
-  state.dialogsPages.messages.push(newMessage);
-  state.dialogsPages.newMessageText = '';
-  rerenderEntireTree(state);
-}
-
-export let updateNewMessageText = (text) => {
-  state.dialogsPages.newMessageText = text;
-  rerenderEntireTree(state);
-}
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
+  
 
 };
 
 
 
-export default state;
+
+window.store = store;
+
+ 
+ 
+
+
+ 
+
+
+
+export default store;
