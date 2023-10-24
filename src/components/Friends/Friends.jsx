821e66e05ce88d89b2538a3fefc2b3cf09dd7636
erentsen <erecenmancaev@gmail.com>
2023-10-24 15:53:14 +0300

    add lib axios and made a request ajax in component Friends

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import classes from "./Friends.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/userM.jpg"
 
 
 
 
 const Friends = (props) => {
   if(props.friends.length === 0) {
-    props.setFriends( [
-      {id : 1,followed: true, name: 'Erentsen', status: 'clear cod', location: {city:'Elista', country: 'Kalmyc'}, avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 2,followed: false, name: 'Saglara', status: 'I`m a boos', location: {city:'Elista', country: 'Kalmyc'}, avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 3,followed: true, name: 'Chingis', status: 'I`m a boss too', location: {city:'Elista', country: 'Kalmyc'}, avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 4,followed: true, name: 'Mama', status: 'tortylan', location: {city:'Elista', country: 'Kalmyc'}, avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'},
-      {id : 5,followed: false, name: 'Papa', status: 'clear cod', location: {city:'Elista', country: 'Kalmyc'}, avatar:'https://cdn2.iconfinder.com/data/icons/danger-problems/512/anonymous-1024.png'}
-    ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
+      props.setFriends(response.data.items);
+
+  })
   }
    return <div>
     {
       props.friends.map( u => <div key={u.id}>
           <div>
             <div>
-              <img className={classes.avatar} src= {u.avatar}/>
+              <img className={classes.avatar} src= {u.photos.small != null ? u.photos.small : userPhoto}/>
             </div>
             <div>
               {u.followed
@@ -33,8 +32,8 @@ const Friends = (props) => {
               <div>{u.status}</div>
             </div>
             <div>
-              <div>{u.location.city}</div>
-              <div>{u.location.country}</div>
+              <div>{'u.location.city'}</div>
+              <div>{'u.location.country'}</div>
             </div>
           </div>
         </div>
